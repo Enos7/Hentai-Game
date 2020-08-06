cb342654ab48fb8fae48655a17170ea82f8b6676
Eliezer Solinger <eliezersolinger@gmail.com>
2020-08-05 23:37:02 -0300

    subindo escopo do programa básico

---
+++ eliezer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    para compilar esse exemplo voce precisa baixar o compilador gcc
+    com o comando "sudo apt install build-essential"
+
+    com o compilador instalado basta rodar "gcc eliezer.c"
+    entao o programa compilado vai aparecer na pasta com o nome "a.out"
+    e para rodar basta executar 
+    ./a.out
+
+*/
+int main() {
+    printf("bem vindo ao meu mundo!\n");
+    return 0;
+}
