a45e5b95d84da1b8bf43d5c0c642f17c9695427d
Eliezer Solinger <eliezersolinger@gmail.com>
2020-08-07 03:32:50 -0300

    adicionei a opção  de escolha  da raça do personagem

--- eliezer.c
+++ eliezer.c
@@ -14,38 +14,85 @@
     "gcc eliezer.c && ./a.out"
 
 */
+/* AQUI VAI AS FUNCOES DE UTILIDADES DO SISTEMA */
 
-char pedir_tecla() {
-    // ler o caracter do usuario, o primeiro character
-    char ch=getchar();
-    // limpar o buffer de entrada
+void limpar_buffer_de_entrada() {
     char c; while ((c = fgetc (stdin)) != EOF && c != '\n');
+}
+char pedir_tecla() {
+    char ch=getchar();
+    limpar_buffer_de_entrada();
     return ch;
 }
-
-char player_nome[200];
-int player_saude=100;
+int pedir_inteiro() {
+    int valor;
+    scanf("%d",&valor);
+    limpar_buffer_de_entrada();
+    return valor;
+}
 
 void limpar_tela() { system("clear");}
 
-void printar_informacoes() {
+/*   AQUI COMEÇA REALMENTE A LOGICA DO JOGO */
+
+typedef struct {
+    char *nome;
+    int saude;
+    int sorte;
+    int dano;
+    int magia;
+} Raca;
+
+Raca racas[]={
+    //nome       saude      sorte       dano        magia       
+    {"Elfo"     ,50         ,110        ,40          ,130   },
+    {"Nordico"  ,100        ,60         ,120         ,40    },
+    {"Orc"      ,150        ,50         ,110         ,20    }
+};
+
+char personagem_nome[200];
+Raca personagem_raca;
+
+void menu_criacao_personagem() {
+    limpar_tela();
+    printf("Bem vindo ao meu mundo!\n");
+    sleep(1); 
+    printf("Para começar digite um nome para o seu personagem:");
+    scanf("%s",personagem_nome);
+    printf("-------RAÇAS----\n");
+    int numero_de_racas=sizeof(racas)/sizeof(Raca);
+    for (int i = 0; i < numero_de_racas; i++){
+        printf("%d) %s \n",i,racas[i].nome);
+    }
+    printf("Escolha a raça: ");
+
+    int raca_escolhida=-1;
+    while (!(raca_escolhida>=0 && raca_escolhida<numero_de_racas)){
+        raca_escolhida=pedir_inteiro();
+    }
+    personagem_raca=racas[raca_escolhida];
+    limpar_tela();
+}
+
+void menu_informacoes() {
     limpar_tela();
     printf("----------INFORMACOES--------\n");
-    printf("Nome do player: %s \n",player_nome);
-    printf("Saude: %i \n",player_saude);
+    printf("Nome do personagem: %s \n",personagem_nome);
+    printf("Raça: %s \n",personagem_raca.nome);
+    printf("Saude: %d \n",personagem_raca.saude);
+    printf("Sorte: %d \n",personagem_raca.sorte);
+    printf("Dano: %d \n",personagem_raca.dano);
+    printf("Magia: %d \n",personagem_raca.magia);
     printf("-----------------------------\n");
 }
 
 int main() {
-    limpar_tela();
-    printf("Bem vindo ao meu mundo!\n");
-    sleep(1); 
-    printf("Para começar digite um nome para o seu player:");
-    scanf("%s",player_nome);
     setlocale(LC_ALL, "");
+    menu_criacao_personagem();
+
     char tecla_pressionada;
     while (1) {
-        printf("digite 'i' para suas mostrar seus dados, 'x' para sair.");
+        printf("digite 'i' para as informações do personagem, 'x' para sair.");
         tecla_pressionada=pedir_tecla();
         if(tecla_pressionada=='x') break;
         if(tecla_pressionada=='i') {
